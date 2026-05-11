e6f1bd5c8eb11a2b490990f2b7db93b286049978
macP <andrea.poletti.94@gmail.com>
2026-05-11 06:55:46 +0200

    update: main now stores messages for future reference

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,10 +11,14 @@ if api_key is None:
 client = genai.Client(api_key = api_key)
 
 def main():
-    print("Hello from ai-agent!")
-    response = client.models.generate_content(model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     if (response.usage_metadata is None) :
         raise RuntimeError("usage metadata not found")
+    print("Hello from ai-agent!")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
 
