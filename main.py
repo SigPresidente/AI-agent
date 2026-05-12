7374091e9a08533f6fdb623c3474cfb03e8379fa
macP <andrea.poletti.94@gmail.com>
2026-05-12 06:46:18 +0200

    update: added "verbose" flag

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ client = genai.Client(api_key = api_key)
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
     if (response.usage_metadata is None) :
         raise RuntimeError("usage metadata not found")
     print("Hello from ai-agent!")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    if (args.verbose is True) :
+        print(f"User prompt: {args.user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response: {response.text}")
 
 
