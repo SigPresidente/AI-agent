3f7d79153c811df58a2620df096e655cdb27f529
macP <andrea.poletti.94@gmail.com>
2026-05-14 06:58:24 +0200

    update: added test unit

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,13 @@
-def get_files_info(working_directory, directory="."):
-    os.path.abspath() #Gets the absolute path of the current working directory
+import os
 
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory) #Returns complete dir path of working scope
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory)) #Fixes target dir
+    
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs #Compares working/target dir
+    if not valid_target_dir :
+        print(f'Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    elif not os.path.isdir(directory) :
+        print(f'Error: "{directory}" is not a directory')
+    else :
+        print(f'Success: "{directory}" is within the working directory') 
