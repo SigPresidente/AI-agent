58739955b6e89bdba4f54ee475b3ab59bcfe3d4b
macP <andrea.poletti.94@gmail.com>
2026-07-05 08:33:30 +0200

    fixed test cases

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -8,13 +8,14 @@ def get_file_content(working_directory: str, file_path: str) -> str :
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs #Compares working/target dir
         if not valid_target_dir :
             return (f'Error: Cannot read "{file_path}" as it is outside the permitted working directory')
-        if not os.path.isdir(file_path) :
+        if not os.path.isfile(target_dir) :
             return (f'Error: File not found or is not a regular file: "{file_path}"')
+        with open(target_dir, "r") as f:
+            file_content_string = f.read(CHAR_LIMIT)
+            if f.read(1):
+                file_content_string += f'[...File "{file_path}" truncated at {CHAR_LIMIT} characters]'
+        return file_content_string
     except Exception as e :
         return (f'Error: {e}')
 
-    with open(file_path, "r") as f:
-        file_content_string = f.read(CHAR_LIMIT)
-        if f.read(1):
-            file_content_string += f'[...File "{file_path}" truncated at {CHAR_LIMIT} characters]'
-        print(f"File content: {file_content_string}")
+    
--- test_get_file_content.py
+++ test_get_file_content.py
@@ -4,7 +4,14 @@ result = get_file_content("calculator", "lorem.txt")
 print(f"lorem.txt length: {len(result)}")
 print(f"lorem.txt truncated: {'truncated' in result}")
 
-get_file_content("calculator", "main.py")
-get_file_content("calculator", "pkg/calculator.py")
-get_file_content("calculator", "/bin/cat")
-get_file_content("calculator", "pkg/does_not_exist.py")
+result = get_file_content("calculator", "main.py")
+print(result)
+
+result = get_file_content("calculator", "pkg/calculator.py")
+print(result)
+
+result = get_file_content("calculator", "/bin/cat")
+print(result)
+
+result = get_file_content("calculator", "pkg/does_not_exist.py")
+print(result)
