bbdee3cebc390abf3c68c8ce06ce5b80daa4c818
macP <andrea.poletti.94@gmail.com>
2026-07-20 08:57:42 +0200

    update: switched to openAI from Gemini

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 import os
 import argparse
 from dotenv import load_dotenv
-from google import genai
-from google.genai import types
+from openai import OpenAI
+from prompts import system_prompt
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
+api_key = os.environ.get("OPENROUTER_API_KEY")
 if api_key is None:
-    raise RuntimeError("gemini api key not found")
-client = genai.Client(api_key = api_key)
+    raise RuntimeError("OpenRouter API key not found")
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages)
-    if (response.usage_metadata is None) :
+    messages = [
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": args.user_prompt},
+    ]
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages,
+        temperature=0,
+    )
+    if response.usage is None:
         raise RuntimeError("usage metadata not found")
-    print("Hello from ai-agent!")
-    if (args.verbose is True) :
-        print(f"User prompt: {args.user_prompt}\nPrompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response: {response.text}")
+    if args.verbose:
+        print(
+            f"User prompt: {args.user_prompt}\n"
+            f"Prompt tokens: {response.usage.prompt_tokens}\n"
+            f"Response tokens: {response.usage.completion_tokens}"
+        )
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
