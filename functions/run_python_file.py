51ef41d0213772f083f6c10cf0a452245cfe174f
macP <andrea.poletti.94@gmail.com>
2026-07-20 08:06:13 +0200

    update: added test cases for run_python_file

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 def run_python_file(working_directory: str, file_path: str, args: list[str] | None = None) -> str:
     try :
@@ -7,15 +8,24 @@ def run_python_file(working_directory: str, file_path: str, args: list[str] | No
         valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
         if not valid_target_dir :
             return (f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory')
-        if not os.path.isfile(file_path) :
+        if not os.path.isfile(target_dir) :
             return (f'Error: "{file_path}" does not exist or is not a regular file')
         if not file_path.endswith('.py') :
             return (f'Error: "{file_path}" is not a Python file')
     except Exception as e :
         return (f'Error: {e}')
     
-    command = ["python", absolute_file_path]
-    command.extend(args)
-    completed_process =subprocess.run(command, *, stdin=None, input=None, stdout=None, stderr=None, capture_output=False, shell=False, cwd=None, timeout=30, check=False, encoding=None, errors=None, text=True, env=None, universal_newlines=None)
-
-    #Proseguire da passaggio 7#
+    try :
+        command = ["python", target_dir]
+        if (args is not None) :
+            command.extend(args)
+        completed_process = subprocess.run(command, stdin=None, input=None, capture_output=True, shell=False, cwd=working_directory, timeout=30, 
+                                       check=False, encoding=None, errors=None, text=True, env=None, universal_newlines=None)
+        if completed_process.returncode != 0 :
+            return (f'Process exited with code {completed_process.returncode}') 
+        elif (completed_process.stdout is None and completed_process.stderr is None) :
+            return ('No output produced')
+        else :
+            return (f'STDOUT: {completed_process.stdout}\nSTDERR: {completed_process.stderr}')
+    except Exception as e :
+        return (f'Error: executing Python file: {e}')
