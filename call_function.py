715d7f41d49bc9977a4ccef4004e8802d23825ea
macP <andrea.poletti.94@gmail.com>
2026-07-29 07:06:29 +0200

    update: added function calling for agent

--- call_function.py
+++ call_function.py
@@ -1,3 +1,6 @@
+import json
+from collections.abc import Callable
+
 from functions.get_file_content import schema_get_file_content
 from functions.get_files_info import schema_get_files_info
 from functions.run_python_file import schema_run_python_file
@@ -9,3 +12,25 @@ available_functions = [
     schema_write_file,
     schema_run_python_file
 ]
+
+def call_function(tool_call, verbose: bool = False) -> dict:
+    function_name = tool_call.function.name
+    function_args = json.loads(tool_call.function.arguments or {"working_directory": "./calculator"})
+
+    if verbose :
+        print(f"Calling function: {function_name}({function_args})")
+    else :
+        print(f" - Calling function: {function_name}")
+
+    function_map: dict[str, Callable[..., str]] = {
+        "get_file_content": get_file_content,
+        "get_files_info": get_files_info,
+        "write_file": write_file,
+        "run_python_file": run_python_file
+    }
+
+    return {
+        "role": "tool",
+        "tool_call_id": tool_call.id,
+        "content": f"Error: Unknown function: {function_name}",
+    }
